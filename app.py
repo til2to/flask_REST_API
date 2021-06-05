e11cc218ec170518f039e9a0d0e2d6510a0bfd6e
Nana <nana@Nana-Mac.local>
2021-06-05 10:02:05 +0000

    removed sqlite for the local running

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+#import re
 
 from flask import Flask
 from flask_restful import Api
@@ -11,7 +12,7 @@ from resources.store import Store, StoreList
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
